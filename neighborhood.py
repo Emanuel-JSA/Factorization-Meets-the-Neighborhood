15fcbfaedad425f649c8e0ec1f017ba37d8b0652
Emanuel-JSA <88892759+Emanuel-JSA@users.noreply.github.com>
2026-05-25 11:54:06 -0300

    similarity between itens

--- neighborhood.py
+++ neighborhood.py
@@ -6,15 +6,18 @@ from sklearn.model_selection import train_test_split
 ratings = pd.read_csv("dataset/ratings.csv")
 ratings.head()
 
-# Criar um df(set) semelhanca[user, movie, semelhanca]
 # Pij Pearson correlation coefficient entre itens
-
 matriz_ui = ratings.pivot_table(index='userId', columns='movieId', values='rating')
 matriz_ui.head()
-sim_matriz = matriz_ui.corr(method='pearson', min_periods=2)
-sim_matriz.head()
-# Nij = numero de user que deram ratings para ambos os itens
-# lambda2 = 100
-# semelhanca = (Nij  / Nij * lambda2)Pij
-# Rui = Bui +
-#
+pearson_matriz = matriz_ui.corr(method='pearson', min_periods=2)
+pearson_matriz.head()
+
+# Nij número de usuários que avaliaram ambos os itens
+mask = matriz_ui.notna()
+n_ij = mask.T.dot(mask)
+n_ij.head()
+
+# Semelhança entre os itens
+lambda2 = 100
+s_matriz = n_ij / (n_ij + lambda2) * pearson_matriz
+s_matriz.head()
