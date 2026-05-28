c51a23bcd3ff8a315e47f896d468b84fe79626cd
Emanuel-JSA <88892759+Emanuel-JSA@users.noreply.github.com>
2026-05-28 15:41:27 -0300

    comment code

--- latent_factor.py
+++ latent_factor.py
@@ -33,22 +33,22 @@ ratings = pd.read_csv("dataset/ratings.csv")
 
 # rui = bui + puTqi
 
+# Dados e montar matriz R
 ratings = pd.read_csv("dataset/ratings.csv")
-
 matrix_r = ratings.pivot_table(index="userId", columns="movieId", values="rating")
 matrix_r.head()
 
+# Mascarar dados de treino
 mask = ~np.isnan(matrix_r.values)
 rows, cols = np.where(mask)
 values = matrix_r.values[rows, cols]
-
 train_idx, test_idx = train_test_split(
     range(len(values)), test_size=0.2, random_state=42
 )
-
 R_train = np.full(matrix_r.shape, np.nan)
 R_train[rows[train_idx], cols[train_idx]] = values[train_idx]
 
+# Usa media geral em vez de zero quando não se tem nota
 mu = np.nanmean(R_train)
 R_centered = R_train - mu
 R_filled = np.nan_to_num(R_centered)
@@ -57,14 +57,15 @@ k = 50
 sparse = csr_matrix(np.nan_to_num(R_filled))
 U, s, Vh = svds(sparse, k=k)
 
-
 U = U[:, ::-1]
 s = s[::-1]
 Vh = Vh[::-1, :]
 
+# P e QT
 P = U @ np.diag(s)  # (n_users, k)
 Qt = Vh  # (k, n_items)
 
+# Previsao
 R_hat = (P @ Qt) + mu
 
 R_hat_df = pd.DataFrame(R_hat, index=matrix_r.index, columns=matrix_r.columns)
@@ -79,3 +80,5 @@ mae = mean_absolute_error(y_true, y_pred)
 
 print(f"\nRMSE: {rmse:.4f}")
 print(f"MAE: {mae:.4f}")
+# RMSE: 1.0112
+# MAE: 0.7957
